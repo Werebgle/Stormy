75ca77502b0f0df051c79cf64f308f8ac9067828
Chris Woodhouse <itstime2gohunting@gmail.com>
2015-11-11 09:55:34 -0500

    Stormy

---
+++ app/src/main/java/com/itstime2gohunting/stormy/ui/DailyForecastActivity.java
@@ -0,0 +1,35 @@
+package com.itstime2gohunting.stormy.ui;
+
+import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Parcelable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+
+import com.itstime2gohunting.stormy.R;
+import com.itstime2gohunting.stormy.adapters.DayAdapter;
+import com.itstime2gohunting.stormy.weather.Day;
+
+import java.util.Arrays;
+
+public class DailyForecastActivity extends ListActivity {
+
+    private Day[] mDays;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_daily_forecast);
+
+        Intent intent = getIntent();
+        Parcelable[] parcelables = intent.getParcelableArrayExtra(MainActivity.DAILY_FORECAST);
+        mDays = Arrays.copyOf(parcelables, parcelables.length, Day[].class);
+
+        DayAdapter adapter = new DayAdapter(this, mDays);
+        setListAdapter(adapter);
+    }
+
+}
---
+++ app/src/main/java/com/itstime2gohunting/stormy/ui/HourlyForecastActivity.java
@@ -0,0 +1,49 @@
+package com.itstime2gohunting.stormy.ui;
+
+import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Parcelable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.itstime2gohunting.stormy.R;
+import com.itstime2gohunting.stormy.adapters.HourAdapter;
+import com.itstime2gohunting.stormy.weather.Hour;
+
+import java.util.Arrays;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class HourlyForecastActivity extends ListActivity {
+
+    private Hour[] mHours;
+
+    @Bind (R.id.recyclerView) RecyclerView mRecyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hourly_forecast);
+
+        ButterKnife.bind(this);
+
+        Intent intent = getIntent();
+        Parcelable[] parcelables = intent.getParcelableArrayExtra(MainActivity.HOURLY_FORECAST);
+        mHours = Arrays.copyOf(parcelables, parcelables.length, Hour[].class);
+
+        HourAdapter adapter = new HourAdapter(mHours);
+        mRecyclerView.setAdapter(adapter);
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(layoutManager);
+
+        mRecyclerView.setHasFixedSize(true);
+    }
+
+
+}
